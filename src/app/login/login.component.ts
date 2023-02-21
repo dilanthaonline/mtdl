e2de38ecf65cf057ec9e155ae98397b019477846
Dilantha Perera <dilantha@silentiuminternational.com.au>
2023-02-21 17:51:00 +0530

    login component update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  title = 'mtdl';
 
+
+   //hiding info box
+   visible:boolean = false
+
+
+   //onclick toggling both
+   onclick()
+   {
+     this.visible = !this.visible
+   }
+
+
+  constructor (private router:Router){}
+  panelOpenState = false;
+
+  loadModule(pageName:string):void{
+    this.router.navigate([`${pageName}`]);
+
+}
 }
